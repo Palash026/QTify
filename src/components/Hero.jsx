6203160d2677afe14c6fcac3b5f5caa54c067e12
APPLE <apple@APPLEs-MacBook-Pro.local>
2023-09-13 10:42:22 -0500

    Added move right and left animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,11 +5,11 @@ const Hero = () => {
   return (
     <div className='hero'>
       <div className="hero__box flex justify-center align-center gap-10 flex-wrap">
-        <div className="hero__heading text-center h-100">
+        <div className="hero__heading text-center h-100 moveInLeft-animation">
           <h1 className="hero__title">100 Thousand Songs, ad-free</h1>
           <h1 className="hero__title">Over Thousands podcast episodes</h1>
         </div>
-        <img src={headphone} alt="Headphone Icon" className="hero__image" />
+        <img src={headphone} alt="Headphone Icon" className="hero__image moveInRight-animation" />
       </div>
     </div>
   );
